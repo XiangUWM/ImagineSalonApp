b2cab4ce280c3a7a8a050462fdbc9758c4af0ec7
GitsAndGlamour <omierwalls@gmail.com>
2016-03-07 09:10:19 -0500

    Continuing Establishing the Library

--- config/router/routes.js
+++ config/router/routes.js
@@ -7,14 +7,26 @@ app.config(['$routeProvider',
             function ($routeProvider) {
         $routeProvider.
         when('/', {
-                //Home Page
-                templateUrl: 'app/view/pages/test.php'
-                // controller: 'homePageController'
-
-            }).
-        when('/test', {
-            templateUrl:
-            'app/view/pages/test.php'
+            //Home Page
+            templateUrl: 'app/view/pages/templates/home.php'
+        }).
+        when('/dashboard', {
+            templateUrl: 'app/view/pages/templates/test.php'
+        }).
+        when('/calendar', {
+            templateUrl: 'app/view/pages/templates/test.php'
+        }).
+        when('/customers', {
+            templateUrl: 'app/view/pages/templates/test.php'
+        }).
+        when('/inventory', {
+            templateUrl: 'app/view/pages/templates/test.php'
+        }).
+        when('/settings', {
+            templateUrl: 'app/view/pages/templates/test.php'
+        }).
+        when('/reports', {
+            templateUrl: 'app/view/pages/templates/test.php'
         }).
         otherwise({
             redirectTo: '/'
