1029f542be0ff23405f1a335f8c48874b4dd82bb
GitsAndGlamour <omierwalls@gmail.com>
2016-03-07 09:11:06 -0500

    Establishing Templates

--- app/view/pages/templates/test.php
+++ app/view/pages/templates/test.php
@@ -1,7 +1,5 @@
-<a href="../../../index.php">Back to home</a>
-
 <?php //Initiate the class
-$path = '../../../config/db/db.php';
+$path = '../../../../config/db/db.php';
 if (file_exists($path)) {
     require($path);
     echo '<script>console.log("database connected");</script>';
