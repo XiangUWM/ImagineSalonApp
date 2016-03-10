c196358c1242bb96ba2587a29f3789245fca069b
GitsAndGlamour <omierwalls@gmail.com>
2016-03-10 01:08:29 -0500

    Documented index.php

--- app/view/pages/main.php
+++ app/view/pages/main.php
@@ -1,9 +1,11 @@
 <!doctype HTML>
+<!-- ng-app - AngularJs Application Declaration ("imagineSalonApp") -->
 <html ng-app="imagineSalonApp">
     <?php require('snippets/head.html'); ?>
 <body>
     <?php require('snippets/header.html'); ?>
     <?php require('snippets/nav.html'); ?>
+<!--  Specify where in the main HTML document to load the templates provided in the routeProvider  -->
     <main ng-view></main>
     <?php require('snippets/footer.html'); ?>
 </body>
--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
-$db_path = 'config/db/db.php';
-$main_path = 'app/view/pages/main.php';
-if (file_exists($db_path)) {
-    require($db_path);
-    echo '<script>console.log("database connected");</script>';
-    if (file_exists($main_path)) {
-        require($main_path);
-        echo '<script>console.log("home page found");</script>';
+$db_path = 'config/db/db.php'; // path to database configuration
+$main_path = 'app/view/pages/main.php'; // path to main HTML document
+if (file_exists($db_path)) { // if the database configuration file exists...
+    
+    require($db_path); // load the database configuration file.
+    echo '<script>console.log("database connected");</script>'; // echo a JavaScript console log confirmation upon success.
+    
+    if (file_exists($main_path)) { // if the main HTML document also exists...
+        require($main_path); // load the main HTML document.
+        echo '<script>console.log("home page found");</script>'; // echo a JavaScript console log confirmation upon success.
     } else {
-        echo '<script>console.log("home page is not found");</script>';
+        echo '<script>console.log("home page is not found");</script>'; // echo an error statement to the console log upon failure to establish main HTML document.
     }
 } else {
-        echo '<script>console.log("database is not connected");</script>';
+        echo '<script>console.log("database is not connected");</script>'; // echo an error statement to the console log upon failure to establish connection with the database. 
 }
 ?>
