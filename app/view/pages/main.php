9c6ac51391aefd5d19c2a81d2261edfaa74ebd2c
GitsAndGlamour <omierwalls@gmail.com>
2016-03-10 01:11:28 -0500

    Documented app/view/pages/main.php

--- app/view/pages/main.php
+++ app/view/pages/main.php
@@ -1,13 +1,13 @@
 <!doctype HTML>
 <!-- ng-app - AngularJs Application Declaration ("imagineSalonApp") -->
 <html ng-app="imagineSalonApp">
-    <?php require('snippets/head.html'); ?>
+    <?php require('snippets/head.html'); ?> // require HEAD content for CSS links and JavaScripting 
 <body>
-    <?php require('snippets/header.html'); ?>
-    <?php require('snippets/nav.html'); ?>
+    <?php require('snippets/header.html'); ?> // require HEADER content for utilities 
+    <?php require('snippets/nav.html'); ?> // require NAVBAR content for primary navigation  
 <!--  Specify where in the main HTML document to load the templates provided in the routeProvider  -->
     <main ng-view></main>
-    <?php require('snippets/footer.html'); ?>
+    <?php require('snippets/footer.html'); ?> // require FOOTER - file is blank
 </body>
 
 </html>
